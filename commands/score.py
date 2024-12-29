50634fbec0b55dacbf08e738dbb6c35903cfc92a
Devilcode53 <japusahu7@gmail.com>
2024-12-29 08:56:18 +0530

    Update score.py

--- commands/score.py
+++ commands/score.py
@@ -1,7 +1,8 @@
 from telethon import events
 
-async def command_handler(event):
-    # Get user information and send a welcome message
-    user = await event.get_sender()
-    await event.reply(f"Hello, {user.first_name}! Welcome to the Word Game Bot! 🎮")
+from games.word_scramble import stop_game
 
+command = "score"
+
+async def handler(event):
+    await event.reply("woking on it !will be added soon.")
